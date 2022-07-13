795e61d8484565e11e18df1b08920a5960d9177b
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-13 11:35:09 -0600

    Add cell render method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -40,4 +40,34 @@ describe Cell do
       expect(cell.ship.health).to eq(2)
       expect(cell.fired_upon?).to eq(true)
     end
+
+    it 'can return the status of a cell' do
+        cell_1 = Cell.new("B4")
+
+        expect(cell_1.render).to eq(".")
+        
+        cell_1.fire_upon
+
+        expect(cell_1.render).to eq("M")
+
+        cell_2 = Cell.new("C3")
+        cruiser = Ship.new("Cruiser", 3)
+
+        cell_2.place_ship(cruiser)
+     
+        expect(cell_2.render).to eq(".")
+        expect(cell_2.render(true)).to eq("S")
+
+        cell_2.fire_upon
+
+        expect(cell_2.render).to eq("H")
+        expect(cruiser.sunk?).to eq(false)
+
+        2.times do cruiser.hit end
+        
+        expect(cruiser.sunk?).to eq(true)
+        expect(cell_2.render).to eq("X")
+
+        end
+
 end
