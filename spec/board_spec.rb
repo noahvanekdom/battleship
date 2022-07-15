3183547c2696cd6ed5264c192a79aa9e56cf7b7d
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-15 11:40:52 -0600

    Add valid placement test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,4 +34,25 @@ describe Board do
     expect(board.valid_coordinate?("E1")).to eq(false)
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
+
+  it 'can tell if a ship placement is valid' 
+    board = Board.new 
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    # CONTEXT: number of coordinates in the array = ship.length
+    expect(board.valid_placement?(cruiser, ["A1","A2"])).to be false
+    expect(board.valid_placement?(submarine, ["A1","A2","A3"])).to be false
+    # CONTEXT: coordinates have to be consecutive
+    expect(board.valid_placement?(cruiser, ["A1","A2","A4"])).to be false
+    expect(board.valid_placement?(submarine, ["A1","C1"])).to be false
+    expect(board.valid_placement?(cruiser, ["A3","A2","A1"])).to be false
+    expect(board.valid_placement?(submarine, ["C1","B1"])).to be false
+    # CONTEXT: coordinates can't be diagonal
+    expect(board.valid_placement?(cruiser, ["A1","B2","C3"])).to be false
+    expect(board.valid_placement?(submarine, ["C2","D3"])).to be false
+    # CONTEXT: when coordinates are valid
+    expect(board.valid_placement?(submarine, ["A1","A2"])).to be true
+    expect(board.valid_placement?(cruiser, ["B1","C1","D1"])).to be true
+
+
 end
